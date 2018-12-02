917d585afe6d535db72cd611ce53a2e91d2fe854
Edouard <e.peninb@gmail.com>
2018-12-02 16:02:43 +0100

    Added fallback module for file imports when using require() with jest
    Requiring css/images/whatever non ts/tsx file fails. This adds path to a fake module returning a string.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupTestFrameworkScriptFile: "<rootDir>/src/testsSetup.ts",
+  	moduleNameMapper: {
+		"\\.(css|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jestEmptyModule.js",
+	},
 };
